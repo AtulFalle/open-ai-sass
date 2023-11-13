947ef38ff464488f6c39bd7e3bd83cf1462368df
Atul <atulfalle007@gmail.com>
2023-11-14 02:01:06 +0530

    updated stripe pricing

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -47,7 +47,7 @@ export async function GET() {
                             name: 'Open AI buddy',
                             description: 'Unlimited AI Generations'
                         },
-                        unit_amount: 2000,
+                        unit_amount: 50000,
                         recurring: {
                             interval: "month"
                         }
